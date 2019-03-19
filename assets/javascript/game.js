67e3da9648622d50e126c1331e4aaa5e534862c8
Non Dewman <Dnewman1934@yahoo.com>
2019-03-18 20:35:25 -0500

    why wont it clear my currentScore var??

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-let picValOne;
+let picValOne; //VARIABLES
 let picValTwo;
 let picValThree;
 let picValFour;
@@ -9,26 +9,44 @@ let currentScore;
 
 //value assign function to the pictures
 
-$(function () {
-    function onWin() { //FUNCTION TO RUN ON WIN,
-        wincount++;
-        picAssign();
-    }
-    function onLoss() { //FUNCTION TO RUN ON LOSS
-        losscount++;
-        picAssign();
-    }
-    if (currentScore === targetNum) {  //WIN LOOSE CONDITIONALS
-        $('#currentScore').text('Your current score is:wiiin');
-    }
-    if (currentScore > targetNum) {
-        $('#currentScore').text('Your current score is:looose ');
-    }
+$(function () { //THIS IS THE ON DOCUMENT FUNCTION
+    let currentScore = 0;
+    let wincount = 0;
+    let losscount = 0;
+
     function picAssign() { //ASSIGNS VALUES, INCLUDES ON BUTTON EVENTS TO ADD TO CURRENT SCORE
         let currentScore = 0;
-        let picValOne = Math.floor((Math.random() * 12) + 1);
+
+
+        function winloose() { //TO RUN ON EACH BUTTON CLICK TO CHECK FOR WIN/LOSS
+
+            console.log('_______');
+            console.log(currentScore);
+            console.log(targetNum);
+            console.log(currentScore === targetNum);
+            console.log(currentScore < targetNum);
+            console.log(currentScore > targetNum);
+            if (currentScore === targetNum) {  //WIN LOOSE CONDITIONALS
+                wincount++;
+                $("#wins").text('Wins: ' + wincount);
+                picAssign();
+                console.log(currentScore);
+                console.log('this is the current score above');
+            }
+            if (currentScore > targetNum) { //AFTER ONE RUN THIS STARTS RUNNING EVERY CLICK?
+                losscount++;
+                console.log(losscount);
+                $("#losses").text('Losses ' + losscount);
+                console.log('reset');
+                console.log(currentScore);
+                picAssign();
+            }
+
+        }
+        let picValOne = Math.floor((Math.random() * 12) + 1); //VALUE ASSIGNING
         $('#picone').val(picValOne);
         console.log(picValOne);
+        console.log(currentScore);
         let picValTwo = Math.floor((Math.random() * 12) + 1);
         console.log(picValTwo);
         $('#pictwo').val(picValTwo);
@@ -40,32 +58,31 @@ $(function () {
         $('#picfour').val(picValFour);
         let targetNum = Math.floor(Math.random() * ((120 - 19) + 1) + 19)
         console.log(targetNum);
-        $('#targetNumber').text('Your current goal is: ' + targetNum);
-        $('#currentScore').text('Your current score is: ' + currentScore);
-        $('#picone').on('click', function () {
-            currentScore += picValOne;
-            $('#currentScore').text('Your current score is: ' + currentScore);
+
+        $('#picone').on('click', function () {  //ADDS VALUES TO CURRNET SCORES ON CLICK,
+            currentScore += picValOne;      //RUNS WINLOOSE FUNCTION
+            $('#currentScore').text('Your current score is: ' + currentScore); //ADDS TO SCORE TEXT
+            winloose();
         })
         $('#pictwo').on('click', function () {
             currentScore += picValTwo;
             $('#currentScore').text('Your current score is: ' + currentScore);
+            winloose();
         })
         $('#picthree').on('click', function () {
             currentScore += picValThree;
             $('#currentScore').text('Your current score is: ' + currentScore);
+            winloose();
         })
         $('#picfour').on('click', function () {
             currentScore += picValFour;
             $('#currentScore').text('Your current score is: ' + currentScore);
-            console.log(currentScore);
-            console.log(targetNum);
+            winloose();
         })
 
+        $('#targetNumber').text('Your current goal is: ' + targetNum); //SETS TEXTS
+        $('#currentScore').text('Your current score is: ' + currentScore);
     }
-
     picAssign(); //RUNS FUNCTION
-
-
-
 });
 
